2d49e2f10db9d6f987213cc4184b9ce2935b3591
Oleg Oleynik <xakep0k@mail.ru>
2019-09-13 12:17:48 +0300

    delete contacts

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 # File.open("public/ciphers_#{Date.today.to_s}.txt","w+") do |f|
 # f.puts(chipers)
 # end
-#TobaccoCipher.destroy_all
+TobaccoCipher.destroy_all
   File.open("public/1/ciphers_2019-03-15.txt").each do |line|
   TobaccoCipher.create(:cipher => line.strip)
 end
